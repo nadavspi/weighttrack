353803f044a420baa31a5337d5c7ec89f82122fb
Nadav Spiegelman <me@nadav.name>
2013-10-10 19:57:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weighttrack::Application.config.secret_key_base = '93b257a80a9ba6b1ed7ca2cd606f02e6816d27bfc11d438bf836d91c321fdf67cdece95c2430ae3ed15708bd3be8e14f6a8aacbfe86e3004e233dfd938fea6ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Weighttrack::Application.config.session_store :cookie_store, key: '_weighttrack_session'
