20903bd92e13d18f1095c7c43540dded12aa3fd6
Nadav Spiegelman <me@nadav.name>
2013-10-11 17:53:23 -0400

    route index to weights#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Weighttrack::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'weights#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
