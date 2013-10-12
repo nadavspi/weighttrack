f8cea31970f0eb612108140c2b69e54b52ae4210
Nadav Spiegelman <me@nadav.name>
2013-10-12 18:09:51 -0400

    Sort by desc date

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -12,6 +12,6 @@
 
 class Weight < ActiveRecord::Base
   belongs_to :user
-
   validates :value, :numericality => {:greater_than_or_equal_to => 35.0, :less_than_or_equal_to => 700.0}
+  default_scope order: 'weights.date DESC'
 end
