9b817726621607b107f1235fd710f61f786d2e84
Nadav Spiegelman <me@nadav.name>
2013-10-12 23:47:52 -0400

    No need to format date

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -2,7 +2,7 @@ class EmailProcessor
   def self.process(email)
     @user = User.find_by_email(email.from)
     if @user
-      @user.weights.create!({value: email.body, date: Time.now.strftime("%m/%d/%Y")})
+      @user.weights.create!({value: email.body, date: Time.now})
     end
   end
 end
