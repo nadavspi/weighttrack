d0019a6effca3282bb61e5432c12eeb272c83bea
Nadav Spiegelman <me@nadav.name>
2013-10-13 00:25:26 -0400

    Comment out time zone filter for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  around_filter :user_time_zone, if: :current_user
+  # around_filter :user_time_zone, if: :current_user
 
     private
 
