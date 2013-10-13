d79427bc5dd8528174ae29371de3ac2b8fba06d3
Nadav Spiegelman <me@nadav.name>
2013-10-12 20:28:08 -0400

    Add time_zone to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,12 @@
 #  encrypted_password :string(128)      not null
 #  confirmation_token :string(128)
 #  remember_token     :string(128)      not null
+#  time_zone          :string(255)
 #
 
 class User < ActiveRecord::Base
   include Clearance::User
   has_many :weights
+
+  validates_inclusion_of :time_zone, in: ActiveSupport::TimeZone.zones_map(&:name)
 end
