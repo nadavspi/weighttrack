1960003c3bbd550939520255726da8e73374f213
Nadav Spiegelman <me@nadav.name>
2013-10-13 13:42:05 -0400

    Validate uniqueness of date

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -12,8 +12,8 @@
 
 class Weight < ActiveRecord::Base
   belongs_to :user
-  validates :value, :numericality => {:greater_than_or_equal_to => 35.0, :less_than_or_equal_to => 700.0}
-  validates :value, :date, presence: true
+  validates :value, :numericality => {:greater_than_or_equal_to => 35.0, :less_than_or_equal_to => 700.0}, presence: true
+  validates :date, presence: true, uniqueness: true
   default_scope order: 'weights.date DESC'
 
   before_validation :default_date
